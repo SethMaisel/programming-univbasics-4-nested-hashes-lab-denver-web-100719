d6a7140b356d72835d48705a8bcb0d36033d1eb5
Seth Maisel <seth.maisel@gmail.com>
2019-10-01 16:58:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -122,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-return programmer_hash[:grace_hopper]
+
 end
