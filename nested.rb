ac777772be486a8892d1e28bf1067c20c8780d11
Seth Maisel <seth.maisel@gmail.com>
2019-10-01 16:53:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	return alan_kay[known_for]
+	puts alan_kay[known_for]
 	
 	programmer_hash = 
  		{
@@ -122,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+return programmer_hash
 end
